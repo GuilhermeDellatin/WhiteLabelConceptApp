b63ec91fbe4ca6746026301d4a84df24cb8a3cda
GuilhermeDellatin <guidellatin@gmail.com>
2021-10-25 20:21:08 -0300

    Create Domain Layer and create model package and Product class

---
+++ app/src/main/java/com/gfdellatin/whitelabelconceptapp/domain/model/Product.kt
@@ -0,0 +1,8 @@
+package com.gfdellatin.whitelabelconceptapp.domain.model
+
+data class Product(
+    val id: String = "",
+    val description: String = "",
+    val price: Double = 0.0,
+    val imageUrl: String =""
+)
