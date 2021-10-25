ebda6feecdd6b0c09a05c456cb077e08edaab77e
GuilhermeDellatin <guidellatin@gmail.com>
2021-10-25 20:26:22 -0300

    Data Layer and create ProductRepository

---
+++ app/src/main/java/com/gfdellatin/whitelabelconceptapp/data/ProductRepository.kt
@@ -0,0 +1,16 @@
+package com.gfdellatin.whitelabelconceptapp.data
+
+import android.net.Uri
+import com.gfdellatin.whitelabelconceptapp.domain.model.Product
+
+class ProductRepository(private val dataSource: ProductDataSource) {
+
+    suspend fun getProducts(): List<Product> = dataSource.getProducts()
+
+    suspend fun uploadProductImage(imageUri: Uri): String =
+        dataSource.uploadProductImage(imageUri)
+
+    suspend fun createProduct(product: Product): Product =
+        dataSource.createProduct(product)
+
+}
