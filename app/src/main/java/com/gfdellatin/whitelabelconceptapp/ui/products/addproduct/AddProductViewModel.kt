a41854e8e71c78b252696b532abf756df3c2b6d8
GuilhermeDellatin <guidellatin@gmail.com>
2021-10-30 00:22:15 -0300

    Fragment add Product and Navigation

---
+++ app/src/main/java/com/gfdellatin/whitelabelconceptapp/ui/products/addproduct/AddProductViewModel.kt
@@ -0,0 +1,7 @@
+package com.gfdellatin.whitelabelconceptapp.ui.products.addproduct
+
+import androidx.lifecycle.ViewModel
+
+class AddProductViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
